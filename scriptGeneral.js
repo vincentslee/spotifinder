d1f9c9698e1e27c9aafc65f6eda48c19319a45ca
Charlene Peters <charlene@peters_services.com>
2020-07-18 13:06:49 -0500

    Adding original files

---
+++ scriptGeneral.js
@@ -0,0 +1,24 @@
+// Search Terms with Results
+
+/* Server-Side APIs - 
+
+JQuery
+<script
+  src="http://code.jquery.com/jquery-3.5.1.slim.js"
+  integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM="
+  crossorigin="anonymous"></script>
+
+  Will be used for search button
+
+  $(#SearchButton).submit("click", songGo);
+
+  function songGo{
+      // Click of button will combine Emotions API, Oxford API and Spotify API to find relevant song
+  }
+
+  Moment JS
+  // Log previous song suggestions and time/date
+
+Get Names of Functions
+
+  */
