892f7ee31f0b6a501e218ae9c87f929434f0b672
vincentslee <64751893+vincentslee@users.noreply.github.com>
2020-07-30 21:35:22 -0500

    Vincent (#9)
    * Adding original files
    
    * Updated file
    
    * Initial Readme
    
    * Created some functions
    
    * Emotion API started to work and cleaned up file
    
    * added youtube search functionality
    
    * Updates
    
    * Created YouTube URL
    
    * updated functionality
    
    * commented out invalid emojis
    
    Co-authored-by: Charlene Peters <charlene@peters_services.com>
    Co-authored-by: petersvirtualservices <47577984+petersvirtualservices@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,94 +1,228 @@
-//Here are the input variables
-//search term to be entered into search api, happy as placeholder
-var search = "happy";
-//amount of results to be displayed, 5 as placeholder
-var resultsamount = 3;
+//Charlene, this is the variable that the web page's results display option will be input to
+var howmany = 5;
+//////////////////////
+$("#happy").on("click", function () {
+    event.preventDefault();
+    //$("#viddisplay").empty();
+    var searchterm = "happy";
+    youtubesearch(searchterm, howmany);
+});
 
-//Here is the youtube search functionality
-var APIKey = "AIzaSyBrb73a647DrHK7GEs3rD1z9MsXgJewZtU";
-var queryURL = "https://www.googleapis.com/youtube/v3/search";
+$("#love").on("click", function () {
+    event.preventDefault();
+    //$("#viddisplay").empty();
+    var searchterm = "love";
+    youtubesearch(searchterm, howmany);
+});
 
-//global variables
-var displayedvideos = [];
-var searchLimit;
+$("#tired").on("click", function () {
+    event.preventDefault();
+    //$("#viddisplay").empty();
+    var searchterm = "tired";
+    youtubesearch(searchterm, howmany);
+});
 
-//youtube api search request
-function repeatedSearch(limit) {
-    if (limit !== resultsamount) {
-        searchLimit = limit;
-    } else {
-        searchLimit = resultsamount;
-    }
+$("#party").on("click", function () {
+    event.preventDefault();
+    //$("#viddisplay").empty();
+    var searchterm = "party";
+    youtubesearch(searchterm, howmany);
+});
+$("#sad").on("click", function () {
+    event.preventDefault();
+    //$("#viddisplay").empty();
+    var searchterm = "sad";
+    youtubesearch(searchterm, howmany);
+});
+$("#angry").on("click", function () {
+    event.preventDefault();
+    //$("#viddisplay").empty();
+    var searchterm = "angry";
+    youtubesearch(searchterm, howmany);
+});
 
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-        data: {
-            key: APIKey,
-            q: search,
-            part: 'snippet',
-            maxResults: searchLimit,
-            type: 'video',
-            videoEmbeddable: true,
-            videoCategoryId: 10}
-    }).then(function(response) {
-        console.log("Got data from Youtube", response);
-        
-        videoarray(response);
-        console.log("displayedvideos", displayedvideos);
-        
-        deleteduplicates(response, displayedvideos);
+$("#workout").on("click", function () {
+    event.preventDefault();
+    //$("#viddisplay").empty();
+    var searchterm = "workout";
+    youtubesearch(searchterm, howmany);
+});
 
-    });
-}
+$("#country").on("click", function () {
+    event.preventDefault();
+    //$("#viddisplay").empty();
+    var searchterm = "country";
+    youtubesearch(searchterm, howmany);
+});
 
-//initial run of youtube search
-repeatedSearch(resultsamount);
+console.log("test");
+// Create function when searchButton is clicked
+$("#searchbutton").on("click", function () {
+    event.preventDefault();
+    //$("#viddisplay").empty();
+    console.log("test");
+    var feelingEntered = $('#searchbar').val();
 
-//creates array of youtube videos
-function videoarray(response){
-    for(var i = 0; i<response.items.length; i++){
-    displayedvideos.push(response.items[i].id.videoId);
-}}
-
-//deltes duplicate videos from list
-function deleteduplicates(response, displayedvideos) {
-    //checks similarities between videos
-    for (var i = 0; i < displayedvideos.length; i++) {
-        for (var k = i + 1; k < displayedvideos.length; k++) {
-
-            if ( stringSimilarity.compareTwoStrings(response.items[i].snippet.title, response.items[k].snippet.title) > 0.5 ) {
-                console.log("deleted"+i+displayedvideos[i]);
-                var displayedvideos = displayedvideos.filter((displayedVideo, index) => index !== i);
-                
-                console.log("filtered array", displayedvideos);
-            }
-
+    console.log(feelingEntered);
+    wordSearchText(feelingEntered);
+});
+  
+  // function search (response) {} = Vincent Youtube search
+  
+function wordSearchText(feelingEntered) {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/",
+        "method": "POST",
+        "headers": {
+            "x-rapidapi-host": "twinword-emotion-analysis-v1.p.rapidapi.com",
+            "x-rapidapi-key": "7941464a45mshe78270a58ac1160p1b1d94jsnf26ae33902b5",
+            "content-type": "application/x-www-form-urlencoded"
+        },
+        "data": {
+            "text": '"' + feelingEntered + '"'
         }
     }
+    $.ajax(settings).done(function (response) {
+        console.log(response)
+        var youTubeCreation;
+        
+        if (response.emotion_scores.anger > 0.01) {
+            youTubeCreation = "anger";
+        }
+        if (response.emotion_scores.disgust > response.emotion_scores.anger) {
+            youTubeCreation = "disgust";
+        }
+        if (response.emotion_scores.fear > response.emotion_scores.disgust) {
+            youTubeCreation = "fear";
+        }
+        if (response.emotion_scores.joy > response.emotion_scores.fear) {
+            youTubeCreation = "joy";
+        }
+        if (response.emotion_scores.sadness > response.emotion_scores.joy) {
+            youTubeCreation = "sadness";
+        }
+        if (response.emotion_scores.surprise > response.emotion_scores.sadness) {
+            youTubeCreation = "surprise";
+        }
+        console.log(youTubeCreation+" youtubecreation");
+        // Create sort function that lists array from highest to lowest
+        
+        var searchterm = youTubeCreation;
+        console.log("youtubesearch");
+        youtubesearch(searchterm, howmany);
+      
+    });
+  };
+
+
+function youtubesearch(searchterm, howmany){
+    $("#viddisplay").empty();
+    var displayedvideos = [];
+    var resultsamount = howmany;
+    var search = searchterm;
+    console.log(search+" working");
+    //Here is the youtube search functionality
     
-    //ends loop if enough unique videos have been put in displayedvideos array
-    if (displayedvideos.length<resultsamount){
-        repeatedSearch((resultsamount-displayedvideos.length)+resultsamount);
-    } else{
-        videodisplay(displayedvideos);
+    //youtube api search request
+    function repeatedSearch(limit) {
+        var searchLimit;
+        var APIKey = "AIzaSyC8qg399n7sQ_el7EAqqF2XEe5h--pU2O4";
+        var queryURL = "https://www.googleapis.com/youtube/v3/search";
+        if (limit !== resultsamount) {
+            searchLimit = limit;
+        } else {
+            searchLimit = resultsamount;
+        }
+
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            data: {
+                key: APIKey,
+                q: search,
+                part: 'snippet',
+                maxResults: searchLimit,
+                type: 'video',
+                videoEmbeddable: true,
+                videoCategoryId: 10}
+        }).then(function(response) {
+            console.log("Got data from Youtube", response);
+            
+            videoarray(response);
+            console.log("displayedvideos", displayedvideos);
+            
+            deleteduplicates(response, displayedvideos);
+
+        });
     }
-    
-}
 
-//displays videos to page
-function videodisplay(displayedvideos){
-    for(var i = 0; i<resultsamount; i++){
-        var vidlink = "https://www.youtube.com/watch?v="+displayedvideos[i];
+    //initial run of youtube search
+    repeatedSearch(resultsamount);
+    //creates array of youtube videos
+    function videoarray(response){
+        for(var i = 0; i<response.items.length; i++){
+        displayedvideos.push(response.items[i].id.videoId);
+    }}
 
-        //These are subject to change based on html structure/layout/design
-        console.log(i,": ", vidlink);
-        $("#results").append("<p>"+vidlink+"</p>");
-        $(".frame").attr("src", "https://www.youtube.com/embed/"+displayedvideos[i]);
-        $("#vid"+(i+1)).attr("src", "https://www.youtube.com/embed/"+displayedvideos[i]);
+    //deltes duplicate videos from list
+    function deleteduplicates(response, displayedvideos) {
+        //checks similarities between videos
+        for (var i = 0; i < displayedvideos.length; i++) {
+            for (var k = i + 1; k < displayedvideos.length; k++) {
+                console.log("title" + response.items[k].snippet.title);
+                if ( stringSimilarity.compareTwoStrings(response.items[i].snippet.title, response.items[k].snippet.title) > 0.5 ) {
+                    console.log("deleted"+i+displayedvideos[i]);
+                    var displayedvideos = displayedvideos.filter((displayedVideo, index) => index !== i);
+                    
+                    console.log("filtered array", displayedvideos);
+                }
+
+            }
+        }
+        
+        //ends loop if enough unique videos have been put in displayedvideos array
+        if (displayedvideos.length<resultsamount){
+            console.log("searching again");
+            repeatedSearch((resultsamount-displayedvideos.length)+resultsamount);
+        }else{
+            console.log("displaying videos");
+            videodisplay(displayedvideos);
+        }
         
     }
+
+    //displays videos to page
+    function videodisplay(displayedvideos){
+        console.log("displaying videos");
+        for(var i = 0; i<resultsamount; i++){
+            var vidlink = "https://www.youtube.com/watch?v="+displayedvideos[i];
+            console.log(i,": ", vidlink);
+
+            $("#viddisplay").append('<div class="is-center pure-u-md-1-4"><iframe src="https://www.youtube.com/embed/'+displayedvideos[i]+'"></iframe></div>');
+            //$("#viddisplay").append('<a href="https://www.youtube.com/watch?v='+displayedvideos[i]+'">link</a>')
+
+            //This is a temporary solution
+            //$("#results").append("<p>"+vidlink+"</p>");
+            //$(".frame").attr("src", "https://www.youtube.com/embed/"+displayedvideos[i]);
+            //$("#vid"+(i+1)).attr("src", "https://www.youtube.com/embed/"+displayedvideos[i]);
+            
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
